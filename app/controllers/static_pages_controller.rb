2580abdb157e60167a06f7fef109ca646a765116
davidlguida@gmail.com <davidlguida@gmail.com>
2014-09-09 01:32:23 -0400

    finish static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def clinics
+  end
+
+  def blog
+  end
+
+  def resources
+  end
+
+  def takeaction
+  end
+end
