924c1291ebc0db3759c9222828de6b0bdbc94381
davidlguida@gmail.com <davidlguida@gmail.com>
2014-09-27 20:13:56 -0400

    add blog

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "World Spine Care"
+    base_title = "WSC"
     if page_title.empty?
       base_title
     else
